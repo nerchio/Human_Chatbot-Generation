cdbed0859b08cd958641431f2fcdd6b3ee774eaf
Ruizhe Zhu <zhurui@student-cluster.inf.ethz.ch>
2025-03-26 00:31:08 +0100

    update

--- Dialogue/main.py
+++ Dialogue/main.py
@@ -7,6 +7,8 @@ from PIL import Image as PILImage
 import io
 from models import *
 from prompts import GET_PROMPT
+import pandas as pd
+from tqdm import tqdm
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -14,31 +16,38 @@ class State(TypedDict):
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
     turns: int
+    inquirer_system_prompt: str
     inquirer_prompt: str
-    responder_prompt: str
+    responder_system_prompt: str
 
 def inquirer(state: State):
-    # system_message = SystemMessage(content="You are an inquirer. Please ask a question about ETHZ.")
+    inquirer_system_prompt = SystemMessage(content=state["inquirer_system_prompt"])
     inquirer_prompt = HumanMessage(content=state["inquirer_prompt"])
-    inquirer_message = state["messages"] + [inquirer_prompt]
-    # print("inquirer_message", inquirer_message)
+    inquirer_message = [inquirer_system_prompt] + state["messages"] + [inquirer_prompt]
     inquirer_response = inquirer_llm.invoke(inquirer_message)
-    # print("llm_response", llm_response)
-    return {"messages": [HumanMessage(content=inquirer_response.content)], "turns": state["turns"] + 1}
+    return {"messages": [HumanMessage(
+        content=inquirer_response.content,
+        additional_kwargs={"source": "generated"}
+    )], "turns": state["turns"] + 1}
 
 def responder(state: State):
-    responder_prompt = SystemMessage(content=state["responder_prompt"])
+    responder_prompt = SystemMessage(content=state["responder_system_prompt"])
     responder_message = [responder_prompt] + state["messages"]
-    return {"messages": [responder_llm.invoke(responder_message)], "turns": state["turns"] + 1}
+    response = responder_llm.invoke(responder_message)
+    return {"messages": [AIMessage(
+        content=response.content,
+        additional_kwargs={"source": "generated"}
+    )], "turns": state["turns"] + 1}
 
 def max_turns_condition(state: State):
-    if state['turns'] >= 7:
+    if state['turns'] >= 10:
         return END
     else:
         return "inquirer"
 
-def proper_content_condition(state: State):
-    if False:
+def content_condition(state: State):
+    content = state["messages"][-1].content
+    if '<EOD>' in content:
         return END
     else:
         return "responder"
@@ -47,40 +56,57 @@ graph_builder = StateGraph(State)
 graph_builder.add_node("inquirer", inquirer)
 graph_builder.add_node("responder", responder)
 
-graph_builder.add_edge(START, "responder")
+graph_builder.add_edge(START, "inquirer")
 graph_builder.add_conditional_edges("responder", max_turns_condition)
-graph_builder.add_conditional_edges("inquirer", proper_content_condition)
+graph_builder.add_conditional_edges("inquirer", content_condition)
 
 graph = graph_builder.compile()
 
-# draw the graph
-# image_bytes = graph.get_graph().draw_mermaid_png()
-# image = PILImage.open(io.BytesIO(image_bytes))
-# image.save("/work/courses/dslab/team3/Human_Chatbot-Generation/Dialogue/graph.png")
+# raw the graph
+image_bytes = graph.get_graph().draw_mermaid_png()
+image = PILImage.open(io.BytesIO(image_bytes))
+image.save("/work/courses/dslab/team3/Human_Chatbot-Generation/Dialogue/graph.png")
 
 
 # INFERENCE PART
 
-inquirer_llm = DeepSeek
+inquirer_llm = GPT4o
 responder_llm = GPT4oMini
-INQUIRER_PROMPT, RESPONDER_PROMPT = GET_PROMPT()
+INQUIRER_SYSTEM_PROMPT, INQUIRER_PROMPT, RESPONDER_SYSTEM_PROMPT = GET_PROMPT()
 
-def stream_graph_updates(user_input: str):
-    print("Human: " + user_input)
-    initial_state = {"messages": [{"role": "user", "content": user_input}], 
-                    "turns": 0, 
-                    "inquirer_prompt": INQUIRER_PROMPT, 
-                    "responder_prompt": RESPONDER_PROMPT}
-    for event in graph.stream(initial_state):
-        for value in event.values():
-            if value["messages"][-1].type == "human":
-                print("(AI) Human:", value["messages"][-1].content)
-            elif value["messages"][-1].type == "ai":
-                print("Chatbot:", value["messages"][-1].content)
-            else:
-                raise ValueError("Unknown message type")
+def graph_update(qa_history: list):
+    messages = []
+    for index, text in enumerate(qa_history):
+        if index % 2 == 0:
+            messages.append(HumanMessage(content=text, additional_kwargs={"source": "qa_history"}))
+        else:
+            messages.append(AIMessage(content=text, additional_kwargs={"source": "qa_history"}))
+    initial_state = {"messages": messages, 
+                    "turns": len(messages), 
+                    "inquirer_system_prompt": INQUIRER_SYSTEM_PROMPT,
+                    "inquirer_prompt": INQUIRER_PROMPT,
+                    "responder_system_prompt": RESPONDER_SYSTEM_PROMPT}
+    final_state = graph.invoke(initial_state)
+    return final_state
 
-        # fallback if input() is not available
+
 if __name__ == "__main__":
-    user_input = "Hello! Could you tell me something about RTX4090?"
-    stream_graph_updates(user_input)
+    df = pd.read_csv('data/qa_test.csv')
+    results = pd.DataFrame(columns=['seed','dialogues'])
+    for index, row in tqdm(df.iterrows(), total=len(df)):
+        qa_history = [row['question'], row['answer']]
+        final_state = graph_update(qa_history)
+        # for message in final_state["messages"]:
+        #     if message.additional_kwargs["source"] == "qa_history":
+        #         if message.type == "human":
+        #             print("Human (Seed): " + message.content)
+        #         elif message.type == "ai":
+        #             print("Chatbot (Seed): " + message.content)
+        #     else:
+        #         if message.type == "human":
+        #             print("Human (Generated): " + message.content)
+        #         elif message.type == "ai":
+        #             print("Chatbot (Generated): " + message.content)
+        results.loc[index] = [qa_history, [m.content for m in final_state["messages"]]]
+    
+    results.to_csv('results/results_qa_test.csv', index=False)
--- Dialogue/prompts.py
+++ Dialogue/prompts.py
@@ -1,9 +1,12 @@
 ROLES = ["Human", "Older", "Younger", "Foreigner"]
 
-INQUIRER_PROMPT = "Please act like you are a human chatting with the chatbot. Say something according to the above context\
-        to complete the conversation. The length limit is 10 words."
+INQUIRER_SYSTEM_PROMPT = "You are a helpful assistant. Please follow the given instructions."
 
-RESPONDER_PROMPT = "You are a helpful assistant. Please answer the question no longer than 10 words."
+INQUIRER_PROMPT = "The conversation above is from a conversation between a human and a chatbot. Now please play the role of the human.\
+Your task is to determine whether the chatbot's answer clarify your initial question clearly and provide detailed information. If it does, please output <EOD>.\
+Otherwise, please comment on the answer and ask a follow-up question to continue the conversation. Do not make statements. Output in human style. Do not output in chatbot style."
+
+RESPONDER_SYSTEM_PROMPT = "You are a helpful assistant. Your output should be no longer than 50 tokens."
 
 def GET_PROMPT(role=0):
-    return INQUIRER_PROMPT, RESPONDER_PROMPT
+    return INQUIRER_SYSTEM_PROMPT, INQUIRER_PROMPT, RESPONDER_SYSTEM_PROMPT
