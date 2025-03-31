9fc32539b62b5dfd9c005d059225b54f17f34a67
Ruizhe Zhu <zhurui@student-cluster.inf.ethz.ch>
2025-03-31 21:29:34 +0200

    update

--- Dialogue/prompts.py
+++ Dialogue/prompts.py
@@ -10,10 +10,11 @@ You are required to act as a human. You are chatting with a chatbot to get some 
 
 INQUIRER_PROMPT = '''
 The conversation above is the chat history between you and the chatbot. 
-Your task is to determine whether the chatbot provides enough knowledge for what you want to know in <task>. 
-If true, please output <\EOD>. Do not output anything else.
+Your task is to determine whether the chatbot provides enough knowledge for what you want to know in <task>. You don't need to provide any analysis. 
+If true, please output "<EOD>". Do not output anything else.
 If false, please output a follow-up question to continue the conversation. 
-** Important: Your output must be in human style instead of chatbot style.**
+
+** Important: Your output must be in human style instead of chatbot style. **
 '''
 
 RESPONDER_SYSTEM_PROMPT = "You are a helpful assistant. Your output should be no longer than 50 tokens."
