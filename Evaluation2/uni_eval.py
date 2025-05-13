a667a5e3e6b06324f8b4c48f8711647fad00358d
haozhu2 <zhuhao010801@outlook.com>
2025-05-13 09:55:29 +0200

    update the uni-eval code

--- Evaluation2/uni_eval.py
+++ Evaluation2/uni_eval.py
@@ -5,13 +5,16 @@ from langgraph.graph.message import add_messages
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 from PIL import Image as PILImage
 import io
+import ast
 # from prompts import GET_EVALUATOER_PROMPT
 
-from evaluation_metrics import uni_eval
+from evaluation_metrics import uni_eval, uni_eval_Deepseek, uni_eval_llama3_2_70B, uni_eval_geminiflash, uni_eval_claude
 
 import re
 
 import json
+
+import sys
 
 
 # Function to read and parse a JSONL file
@@ -28,13 +31,16 @@ def parse_jsonl(file_path):
 
 if __name__ == "__main__":
 
-    dialogue_data_file_names = ["oasst1_en_DeepSeek_GPT4oMini_6.jsonl", "oasst1_en_GPT4oMini_GPT4oMini_6.jsonl", "oasst1_en_GPT4o_GPT4oMini_6.jsonl", "oasst1_en_llama_3B_GPT4oMini_6.jsonl", "oasst1_en_llama_70B_GPT4oMini_6.jsonl", "oasst1_en_min_6_turns_summary.jsonl"]
+    # dialogue_data_file_names = ["oasst1_en_DeepSeek_GPT4oMini_6.jsonl", "oasst1_en_GPT4oMini_GPT4oMini_6.jsonl", "oasst1_en_GPT4o_GPT4oMini_6.jsonl", "oasst1_en_llama_3B_GPT4oMini_6.jsonl", "oasst1_en_llama_70B_GPT4oMini_6.jsonl", "oasst1_en_min_6_turns_summary.jsonl"]
+
+    dialogue_data_file_names = ["arena_DeepSeek_GPT4oMini_10.jsonl", "arena_GPT4oMini_GPT4oMini_10.jsonl", "arena_GPT4o_GPT4oMini_10.jsonl", "arena_llama_3B_GPT4oMini_10.jsonl", "arena_llama_70B_GPT4oMini_10.jsonl", "arena_mistral_7B_GPT4oMini_10.jsonl", "arena_model_a_summaries.jsonl"]
+    
 
     for dialogue_data_file_name in dialogue_data_file_names:
 
-        dialogue_data_path = "/home/haozhu2/Human_Chatbot-Generation/Evaluation/data/" + dialogue_data_file_name
+        dialogue_data_path = "/home/haozhu2/Human_Chatbot-Generation/Evaluation/data_arena/" + dialogue_data_file_name
 
-        saved_result_file_path = "/home/haozhu2/Human_Chatbot-Generation/Evaluation2/result/uni_eval/" + dialogue_data_file_name
+        saved_result_file_path = "/home/haozhu2/Human_Chatbot-Generation/Evaluation2/result_arena/GeminiFlash_Evaluator/uni_eval/" + dialogue_data_file_name
         saved_result_file = open(saved_result_file_path, "w", encoding="utf-8")
 
         dialogue_data = parse_jsonl(dialogue_data_path)
@@ -54,15 +60,82 @@ if __name__ == "__main__":
 
             # print(type(conversation[0]))
 
-            uni_eval_response = uni_eval(conversation, "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", "gpt-4o")
+            # uni_eval_response = uni_eval(conversation, "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", "gpt-4o")
+            # json_obj = json.loads(uni_eval_response)
+            # saved_result_file.write(json.dumps(json_obj) + "\n")
 
-            
-            # print(uni_eval_response)
 
-            # print(type(uni_eval_response))
 
-            json_obj = json.loads(uni_eval_response)
-            saved_result_file.write(json.dumps(json_obj) + "\n")
+            # uni_eval_DeepSeek_response = uni_eval_Deepseek(conversation)
+            # raw_content = uni_eval_DeepSeek_response.content
+
+            # # print(raw_content)
+
+            # # Extract JSON block safely
+            # match = re.search(r'\{.*\}', raw_content, re.DOTALL)
+            # if match:
+            #     try:
+            #         json_str = match.group(0)
+            #         parsed_json = json.loads(json_str)
+            #         print(parsed_json)
+            #         saved_result_file.write(json.dumps(parsed_json) + "\n")
+            #     except:
+            #         parsed_json = ast.literal_eval(json_str)
+            #         print(parsed_json)
+            #         saved_result_file.write(json.dumps(parsed_json) + "\n")
+            # else:
+            #     print("No JSON object found.")
+
+
+            # uni_eval_llama_70B_response = uni_eval_llama3_2_70B(conversation)
+
+            # # print(uni_eval_llama_70B_response)
+            # json_obj = json.loads(uni_eval_llama_70B_response)
+            # saved_result_file.write(json.dumps(json_obj) + "\n")
+
+
+            uni_eval_geminiflash_response = uni_eval_geminiflash(conversation)
+
+            raw_content = uni_eval_geminiflash_response.content
+
+
+            # Extract JSON block safely
+            match = re.search(r'\{.*\}', raw_content, re.DOTALL)
+            if match:
+                try:
+                    json_str = match.group(0)
+                    parsed_json = json.loads(json_str)
+                    print(parsed_json)
+                    saved_result_file.write(json.dumps(parsed_json) + "\n")
+                except:
+                    parsed_json = ast.literal_eval(json_str)
+                    print(parsed_json)
+                    saved_result_file.write(json.dumps(parsed_json) + "\n")
+            else:
+                sys.exit("No JSON object found.")
+                print("No JSON object found.")
+
+
+
+
+
+            # uni_eval_claude_response = uni_eval_claude(conversation)
+
+            # # print(uni_eval_claude_response)
+
+            # raw_content = uni_eval_claude_response.content
+
+            # # Extract JSON block safely
+            # match = re.search(r'\{.*\}', raw_content, re.DOTALL)
+            # if match:
+            #     json_str = match.group(0)
+            #     parsed_json = json.loads(json_str)
+            #     print(parsed_json)
+            #     saved_result_file.write(json.dumps(parsed_json) + "\n")
+            # else:
+            #     print("No JSON object found.")
+
+
 
 
 
